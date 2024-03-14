b8bedd7302779213172ce7efebf01ce996860877
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-03-14 14:12:39 +0800

    Added Spread but not finished

---
+++ 05-Spread/spread.js
@@ -0,0 +1,10 @@
+// Spread Operator...
+// Allows iterable to spread/expand individually inside receiver
+// Split into single items and COPY them
+
+const udemy = "udemy";
+
+const boy = ["Vlahd", "Jarom", "Jolu", "John", "Kiko"];
+const girls = ["Bernadine", "Wrixie", "Angel", "Bernadette"];
+
+const bestfriend = "Johnny";
